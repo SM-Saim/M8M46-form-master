510a22cf5bd8c5a09980d40cf014a651b702b3b4
SM-Saim <saim777m@example.com>
2023-09-21 12:37:16 +0600

    useRef applied

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import "./App.css";
-import SimpleForm from "./components/SimpleForm/SimpleForm";
-import StatefulForm from "./components/StatefulForm/StatefulForm";
+import RefForm from "./components/RefForm/refForm";
+// import SimpleForm from "./components/SimpleForm/SimpleForm";
+// import StatefulForm from "./components/StatefulForm/StatefulForm";
 
 function App() {
   return (
     <>
       <h1>Vite + React</h1>
       {/* <SimpleForm></SimpleForm> */}
-      <StatefulForm></StatefulForm>
+      {/* <StatefulForm></StatefulForm> */}
+      <RefForm></RefForm>
     </>
   );
 }
--- src/components/StatefulForm/StatefulForm.jsx
+++ src/components/StatefulForm/StatefulForm.jsx
@@ -21,7 +21,7 @@ const StatefulForm = () => {
   };
 
   const handleEmailChange = (e) => {
-    console.log(e.target.value);
+    // console.log(e.target.value);
     setEmail(e.target.value);
   };
 
